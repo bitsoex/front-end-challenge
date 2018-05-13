52b6dafdf42da5be41d8b252ef9bfba6a66cf5a9
Michael Villalba Sotelo <visomi.dev@gmail.com>
2018-05-13 00:36:59 -0500

    added index css and basic style

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 
-import store from '../store'
-import Router from '../router'
+import store from '../../store'
+import Router from '../../router'
+import './index.css'
 
 class App extends Component {
   render () {

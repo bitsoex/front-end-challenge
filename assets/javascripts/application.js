b2e397fd74bdc16618b62acb2eb2b6e306318cc3
Carlos <carlos@frontend.ninja>
2018-05-14 23:16:19 -0500

    Add: [Header] Styles progress.

---
+++ assets/javascripts/application.js
@@ -0,0 +1,9 @@
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+}

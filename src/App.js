eb70d72d75b31f8b689c7b99106a23b678a0c20f
Daniel Sanchez <jose.sanchez@ironbit.com.mx>
2018-05-14 20:37:41 -0500

    useless code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React, { Component } from "react";
+import { timeParse } from "d3-time-format";
 import logo from "./logo.svg";
 import "./App.css";
+import { availableBooks, ticker, trade } from "./api";
+import Candles from "./components/CandlestickChart";
 
 class App extends Component {
+  state = {
+    loading: true,
+    data: []
+  };
+
+  async componentDidMount() {
+    await availableBooks();
+    await ticker("btc_mxn");
+    const data = await this.getData();
+    this.setState({
+      loading: false,
+      data
+    });
+  }
+
+  getData = async () => {
+    const parseDate = timeParse("%Y-%m-%d");
+    const data = await trade({ book: "btc_mxn" });
+    return data.map(d => ({
+      date: parseDate(d.date),
+      open: +d.open,
+      high: +d.high,
+      low: +d.low,
+      close: +d.close,
+      volume: +d.volume
+    }));
+  };
+
   render() {
+    const { loading } = this.state;
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    const { data } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +50,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Candles type="svg" data={data} width={100} />
       </div>
     );
   }
--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,31 @@
 const baseUrl = "https://api.bitso.com/v3";
+const tradeChartUrl = "https://bitso.com/trade/chartJSON";
 const availableBooksUrl = `${baseUrl}/available_books/`;
 const tickerUrl = `${baseUrl}/ticker/`;
 
-// const headers = new Headers({
-//   "Content-Type": "application/json",
-//   "Access-Control-Allow-Origin": "*"
-// });
-
-let headers = new Headers({
-  "Access-Control-Allow-Origin": "",
+const headers = new Headers({
+  "Access-Control-Allow-Origin": "*",
   "Content-Type": "application/json"
 });
 
-export const availableBooks = async () => {
-  const result = await fetch(availableBooksUrl);
-  return await result.json();
-};
+const request = url =>
+  fetch(url, { headers }).then(response => response.json());
 
-export const ticker = async book => {
-  const params = `book=${book}`;
-  console.log("ticker api url", `${tickerUrl}?${params}`);
-  const result = await fetch(`https://api.bitso.com/v3/ticker/`, {
-    mode: "no-cors"
-  });
-  return await result.json();
-};
+export const availableBooks = async () => await request(availableBooksUrl);
+
+/**
+ *
+ * @param {string} book
+ *  the book to look for, example "btc_mxn"
+ */
+export const ticker = async (book = "") =>
+  await request(`${tickerUrl}?book=${book}`);
+
+/**
+ * @param {object} params
+ *
+ * @param {string} params.book
+ * @param {string} params.timeFrame accepted values: 1month, 3months, 1year
+ */
+export const trade = async ({ book = "btc_mxn", timeFrame = "1month" }) =>
+  await request(`${tradeChartUrl}/${book}/${timeFrame}/`);

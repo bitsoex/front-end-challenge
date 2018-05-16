9949e5b42f06fdb39a413562de2a8587ca26f728
Juan Carlos Alvarado <alvarado.carricarte@gmail.com>
2018-05-16 06:44:31 -0700

    Heade Bar v1

--- jcalvarad/src/components/header_bar/HeaderBar.js
+++ jcalvarad/src/components/header_bar/HeaderBar.js
@@ -3,15 +3,32 @@ import AppBar from 'react-toolbox/lib/app_bar/AppBar';
 import Navigation from 'react-toolbox/lib/navigation/Navigation';
 import Link from 'react-toolbox/lib/link/Link';
 import BitsoLogo from '../../assets/images/SVG/bitso_logo.svg'
+import IconMenu from 'react-toolbox/lib/menu/Menu';
+import MenuItem from 'react-toolbox/lib/menu/MenuItem';
+// import {IconMenu, MenuItem, MenuDivider } from 'react-toolbox/lib/menu';
+import styles from './HeaderBar.module.css';
 
 class HeaderBar extends Component {
+    state = {
+        dark: true
+    };
+
+    handleChange = (field, value) => {
+        this.setState({ ...this.state, [field]: value });
+    };
     render() {
         return (
-            <div className="HeaderBar">
-                <AppBar >
-                    <img height='50%' style={{paddingRight:'10px',borderRight:'1px solid #FFFF',height:'50%'}} src={BitsoLogo}/>
-                    <span> EXCHANGE </span>
-                    <Navigation height='10' type='horizontal'>
+            <div className='HeaderBar'>
+            <IconMenu icon='more_vert' position='topLeft' menuRipple>
+                            <MenuItem value='download' icon='get_app' caption='Download' />
+                            <MenuItem value='help' icon='favorite' caption='Favorite' />
+                        </IconMenu>
+                <AppBar>
+                    <img className={styles.logo} src={BitsoLogo} />
+                    <span className={styles['separator-sm']} />
+                    <span className={styles.title}> EXCHANGE </span>
+                    <Navigation className={styles.nav} type='horizontal'>
+                        
                         <Link href='http://' label='Inbox' icon='inbox' />
                         <Link href='http://' active label='Profile' icon='person' />
                     </Navigation>

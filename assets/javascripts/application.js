0fc936e8f5ddad4bb83e5476741ddee8c883553b
Carlos <carlos@frontend.ninja>
2018-05-16 21:08:34 -0500

    Add: [Header] Responsive design Add: [Mobile] Fixed footer with buttons. Add: [Night Mode] Javascript Behaviour.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,3 +1,24 @@
+/* Night Mode Switch Handler
+----------------------------------------- */
+function _toggleNightMode(){
+
+  /* Toggle Night Mode */
+  document.body.classList.toggle('night-mode');
+
+  this.classList.toggle('on');
+
+};
+
+var NightModeSwitchHandler = function(){
+
+  var nightModeBtn = document.getElementById('night-mode');
+
+  /* Add Event Handler */
+  nightModeBtn.addEventListener('click', _toggleNightMode, false);
+};
+
+
+
 function w3_open() {
   document.getElementById("main").style.marginLeft = "25%";
   document.getElementById("mySidebar").style.width = "25%";
@@ -7,3 +28,13 @@ function w3_close() {
   document.getElementById("main").style.marginLeft = "0%";
   document.getElementById("mySidebar").style.display = "none";
 }
+
+var App = function(){
+  "use strict";
+
+  return{
+    init: function(){
+      NightModeSwitchHandler();
+    }
+  };
+}();

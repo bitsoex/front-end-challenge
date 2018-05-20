efdc4eada544da8ff929afbcdea0436b0e67cdce
Tonatiuh <Tonatiuh@DESKTOP-VMTH8NQ>
2018-05-20 14:05:04 -0500

    Vias de tabla de traders

---
+++ src/components/RowTrader.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import NumberFormat from 'react-number-format';
+import Moment from 'react-moment';
+
+class RowTrader extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hover: false,
+        }
+    }
+
+    onOut() {
+        this.setState({hover: false});
+    }
+
+    onOver () {
+        this.setState({hover: true});
+    }
+
+    getPriceColor () {
+        if (this.props.makerSide==='buy') {
+            if (this.state.hover) return this.props.theme.greenLight;
+            else return this.props.theme.greenDark;
+        } else {
+            if (this.state.hover) return this.props.theme.redLight;
+                else return this.props.theme.redDark;
+        }
+    }
+
+    render () {
+        
+        return (
+            <div className='bodyTableTraders' 
+                    style={{ fontWeight: 'lighter' , paddingTop: '4px', paddingBottom: '4px', 
+                        backgroundColor: this.state.hover?this.props.theme.headerMarketTitle:this.props.theme.bodyExchangeColor, 
+                        }} 
+                    onMouseOver={()=>this.onOver()} onMouseOut={()=>this.onOut()} > 
+                <div style={{display:'inline-block', width: '3em', color: this.state.hover?'white':this.props.theme.grayDark}}>
+                    <Moment format="HH:mm:ss">{this.props.hour}</Moment>
+                </div>
+                <div style={{display:'inline-block', width: '8em', textAlign: 'right', 
+                        color: this.getPriceColor() , mixBlendMode: this.state.hover?'screen':'initial' }}>
+                    <NumberFormat value={this.props.price}
+                                displayType={'text'} thousandSeparator={true} decimalScale={ this.props.currency==='MXN'?2:8}  />
+                </div>
+                <div style={{display:'inline-block', width: '8em', textAlign: 'right', color: this.state.hover?'white':this.props.theme.blueLight}}>
+                    {this.props.amount}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default RowTrader;

6a4199632bcfdbe50aa7959ebe0167d545252232
Michael Villalba Sotelo <visomi.dev@gmail.com>
2018-05-23 03:38:18 -0500

    added table component @TODO add styles

--- src/components/ui/Table/index.jsx
+++ src/components/ui/Table/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import classnames from 'classnames'
+import get from 'lodash/get'
 
 import style from './index.css'
 
-const Table = ({ className, data = [], columns = [], header = '' }) => (
+const EMPTY_DATA = 0
+
+function composeTdKey (row, rowIndex, field, fieldIndex) {
+  if (row.id && field.id) return `${row.id}.${field.id}`
+  if (row.id && !field.id) return `${row.id}.${fieldIndex}`
+  if (!row.id && field.id) return `${rowIndex}.${field.id}`
+  return `${rowIndex}.${fieldIndex}`
+}
+
+function printField (field, row) {
+  if (typeof field.accessor === 'function') return field.accessor(row)
+  if (typeof field.accessor === 'string') return get(row, field.accessor, '')
+  if (field.id) return get(row, field.id, '')
+  return ''
+}
+
+const Table = ({ className, data = [], columns = [], header = '', loading = false }) => (
   <div className={classnames(style.table, className)}>
     <div className={style.header}>{ header }</div>
     <div className={style.container}>
@@ -17,37 +34,26 @@ const Table = ({ className, data = [], columns = [], header = '' }) => (
             }
           </tr>
         </thead>
+        <tbody>
+          {
+            data.length === EMPTY_DATA ? (
+              <td colSpan={40} className={classnames({ loading })}>Sin datos</td>
+            ) : data.map((row, rowIndex) => (
+              <tr key={row.id || rowIndex}>
+                {
+                  columns.map((field, fieldIndex) => (
+                    <td key={composeTdKey(row, rowIndex, field, fieldIndex)}>
+                      {printField(field, row)}
+                    </td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
     </div>
   </div>
 )
 
 export default Table
-
-// <table>
-// <thead>
-//   <tr>
-//     <template v-for="(column, index) in columns">
-//       <th :key="column.id || index">{{ column.header }}</th>
-//     </template>
-//     <slot name="column-header"></slot>
-//   </tr>
-// </thead>
-// <tbody>
-//   <tr v-if="data.length === emptyData || loading">
-//     <td colspan="40" :class="{ loading, empty: data.length === emptyData }">
-//       Sin datos
-//     </td>
-//   </tr>
-//   <template v-else v-for="(row, rowIndex) in data">
-//     <tr :key="row.id || rowIndex">
-//       <template v-for="(field, fieldIndex) in columns">
-//         <td :key="composeTdKey(row, rowIndex, field, fieldIndex)">
-//           {{ printFieldData(field, row) }}
-//         </td>
-//       </template>
-//       <slot name="column" :row="row"></slot>
-//     </tr>
-//   </template>
-// </tbody>
-// </table>
--- src/views/Exchange.jsx
+++ src/views/Exchange.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
 import TheHeader from '../components/TheHeader'
+import Table from '../components/ui/Table'
+
+const columns = [
+  { header: 'hora', accessor: 'time' },
+  { header: 'mxn precio', accessor: 'price' },
+  { header: 'btc monto', accessor: 'amount' }
+]
+
+const data = [
+  { time: '123', price: '0', amount: '123123' },
+  { time: '123', price: '0', amount: '123123' },
+  { time: '123', price: '0', amount: '123123' }
+]
 
 export default class Home extends Component {
   render () {
     return (
       <div className='page'>
         <TheHeader page={this.props.page} exchange={100000.12} />
+        <Table
+          header='últimos trades'
+          columns={columns}
+          data={data}
+        />
         <main>
           hello world
         </main>

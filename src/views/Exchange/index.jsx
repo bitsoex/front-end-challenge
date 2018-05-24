08a37ddfe08454d9096650f671a5c354e94c275a
Michael Villalba Sotelo <visomi.dev@gmail.com>
2018-05-24 02:05:30 -0500

    change configuration of babel

--- src/views/Exchange/index.jsx
+++ src/views/Exchange/index.jsx
@@ -2,46 +2,33 @@ import React, { Component } from 'react'
 import TheHeader from '../../components/TheHeader'
 import Table from '../../components/ui/Table'
 
+import { lastTradesData } from '../../../.hardcode'
+
 import './index.css'
 
-const columns = [
-  {
-    header: 'hora',
-    accessor: 'time',
-    className: 'time'
-  },
-  {
-    header: <div className='last-trades-header'><span>mxn</span>precio</div>,
-    className: 'price',
-    accessor: (row) => (
-      <span className={row.price > 0 ? 'revenue' : 'loss'}>
-        {row.price.toLocaleString({ style: 'currency', currency: 'MXN', minimumFractionDigits: 2 })}
-      </span>
-    )
-  },
-  {
-    header: <div className='last-trades-header'><span>btc</span>monto</div>,
-    className: 'amount',
-    accessor: 'amount'
-  }
-]
-
-const data = [
-  { time: '16:10:25', price: 390192, amount: 1.9098 },
-  { time: '16:10:25', price: 390192, amount: 1.9098 },
-  { time: '16:10:25', price: 390192, amount: 1.9098 },
-  { time: '16:10:25', price: -390192, amount: 1.9098 },
-  { time: '16:10:25', price: -390192, amount: 1.9098 },
-  { time: '16:10:25', price: 390192, amount: 1.9098 },
-  { time: '16:10:25', price: -390192, amount: 1.9098 },
-  { time: '16:10:25', price: 390192, amount: 1.9098 },
-  { time: '16:10:25', price: 390192, amount: 1.9098 },
-  { time: '16:10:25', price: 390192, amount: 1.9098 },
-  { time: '16:10:25', price: 390192, amount: 1.9098 },
-  { time: '16:10:25', price: 390192, amount: 1.9098 }
-]
-
 export default class Home extends Component {
+  lastTradesColumns = [
+    {
+      header: 'hora',
+      accessor: 'time',
+      className: 'time'
+    },
+    {
+      header: <div className='last-trades-header'><span>mxn</span>precio</div>,
+      className: 'price',
+      accessor: (row) => (
+        <span className={row.price > 0 ? 'revenue' : 'loss'}>
+          {row.price.toLocaleString({ style: 'currency', currency: 'MXN', minimumFractionDigits: 2 })}
+        </span>
+      )
+    },
+    {
+      header: <div className='last-trades-header'><span>btc</span>monto</div>,
+      className: 'amount',
+      accessor: 'amount'
+    }
+  ]
+
   render () {
     return (
       <div className='page'>
@@ -50,9 +37,12 @@ export default class Home extends Component {
           <Table
             className='last-trades'
             header='últimos trades'
-            columns={columns}
-            data={data}
+            columns={this.lastTradesColumns}
+            data={lastTradesData}
           />
+          <div>
+            <div className='purchase-position' />
+          </div>
         </main>
       </div>
     )

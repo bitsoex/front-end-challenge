e2485718c90688389ee340b49cc826ef7a74cb1f
Michael Villalba Sotelo <visomi.dev@gmail.com>
2018-05-25 05:25:30 -0500

    added sell position table style

--- src/views/Exchange/index.jsx
+++ src/views/Exchange/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import TheHeader from '../../components/TheHeader'
 import Table from '../../components/ui/Table'
 
-import { lastTradesData } from '../../../.hardcode'
+import { lastTradesData, purchasePositionData } from '../../../.hardcode'
 
 import './index.css'
 
@@ -29,6 +29,57 @@ export default class Home extends Component {
     }
   ]
 
+  positionsColumns = [
+    {
+      header: '',
+      accessor: (row) => (
+        <div className='percent' style={{ width: `${row.percent * 100}%` }} />
+      )
+    },
+    {
+      header: 'sum',
+      accessor: 'sum'
+    },
+    {
+      header: <div className='btc-amount'><span>btc</span>monto</div>,
+      className: 'amount',
+      accessor: 'btcAmount'
+    },
+    {
+      header: <div className='mxn-value'><span>mxn</span>valor</div>,
+      className: 'mxn-value',
+      accessor: (row) => row.mxnValue.toLocaleString({ style: 'currency', currency: 'MXN', minimumFractionDigits: 2 })
+    },
+    {
+      header: <div className='mxn-price'><span>mxn</span>precio</div>,
+      className: 'mxn-price',
+      accessor: (row) => row.mxnPrice.toLocaleString({ style: 'currency', currency: 'MXN', minimumFractionDigits: 2 })
+    }
+  ]
+
+  purchasePositionColumns = this.positionsColumns
+  sellPositionColumns = this.positionsColumns.map(value => value).reverse()
+
+  purshasePositionHeader = (
+    <div className='purchase-position-header'>
+      <div className='title'>posturas de compra</div>
+      <div className='average'>
+        <span>mxn</span>
+        bid 319,140.00
+      </div>
+    </div>
+  )
+
+  sellPositionHeader = (
+    <div className='sell-position-header'>
+      <div className='average'>
+        bid 319,140.00
+        <span>mxn</span>
+      </div>
+      <div className='title'>posturas de venta</div>
+    </div>
+  )
+
   render () {
     return (
       <div className='page'>
@@ -41,7 +92,20 @@ export default class Home extends Component {
             data={lastTradesData}
           />
           <div>
-            <div className='purchase-position' />
+            <div className='positions'>
+              <Table
+                className='purchase-position'
+                header={this.purshasePositionHeader}
+                columns={this.purchasePositionColumns}
+                data={purchasePositionData}
+              />
+              <Table
+                className='sell-position'
+                header={this.sellPositionHeader}
+                columns={this.sellPositionColumns}
+                data={purchasePositionData}
+              />
+            </div>
           </div>
         </main>
       </div>

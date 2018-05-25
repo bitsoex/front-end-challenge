8741d40b6b3c6c43e0b9289c1c60d0357af93023
Michael Villalba Sotelo <visomi.dev@gmail.com>
2018-05-25 16:00:11 -0500

    added redux thunk for async actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
 import reducers from './reducers'
 
 export default createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
 )

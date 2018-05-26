29bc77b1c61b55ae3496f4fce6d8bf3aa4c1dafb
Michael Villalba Sotelo <visomi.dev@gmail.com>
2018-05-25 22:07:08 -0500

    added dynamic table headers

--- src/views/Exchange/index.jsx
+++ src/views/Exchange/index.jsx
@@ -10,6 +10,7 @@ import Table from '../../components/ui/Table'
 import { lastTradesData, purchasePositionData } from '../../../.hardcode'
 
 import { getLatestTrades as getLatestTradesAction } from '../../store/actions/exchange'
+import { floatStringToLocaleString } from '../../lib/utils'
 
 import './index.css'
 
@@ -32,81 +33,102 @@ class Home extends Component {
     })
   }
 
-  lastTradesColumns = [
-    {
-      header: 'hora',
-      accessor: 'time',
-      className: 'time'
-    },
-    {
-      header: <div className='last-trades-header'><span>mxn</span>precio</div>,
-      className: 'price',
-      accessor: (row) => (
-        <span className={row.price > 0 ? 'revenue' : 'loss'}>
-          {row.price.toLocaleString({ style: 'currency', currency: 'MXN', minimumFractionDigits: 2 })}
-        </span>
-      )
-    },
-    {
-      header: <div className='last-trades-header'><span>btc</span>monto</div>,
-      className: 'amount',
-      accessor: 'amount'
-    }
-  ]
+  lastTradesColumns () {
+    const [ type, currency ] = this.props.selectedBook.book.split('_')
 
-  positionsColumns = [
-    {
-      header: '',
-      className: 'percent',
-      accessor: (row) => (
-        <div className='percent' style={{ width: `${row.percent * 100}%` }} />
-      )
-    },
-    {
-      header: 'sum',
-      accessor: 'sum'
-    },
-    {
-      header: <div className='btc-amount'><span>btc</span>monto</div>,
-      className: 'amount',
-      accessor: 'btcAmount'
-    },
-    {
-      header: <div className='mxn-value'><span>mxn</span>valor</div>,
-      className: 'mxn-value',
-      accessor: (row) => row.mxnValue.toLocaleString({ style: 'currency', currency: 'MXN', minimumFractionDigits: 2 })
-    },
-    {
-      header: <div className='mxn-price'><span>mxn</span>precio</div>,
-      className: 'mxn-price',
-      accessor: (row) => row.mxnPrice.toLocaleString({ style: 'currency', currency: 'MXN', minimumFractionDigits: 2 })
-    }
-  ]
+    return [
+      {
+        header: 'hora',
+        accessor: 'time',
+        className: 'time'
+      },
+      {
+        header: <div className='last-trades-header'><span>{currency}</span>precio</div>,
+        className: 'price',
+        accessor: (row) => (
+          <span className={row.price > 0 ? 'revenue' : 'loss'}>
+            {row.price.toLocaleString({ style: 'currency', currency: currency, minimumFractionDigits: 2 })}
+          </span>
+        )
+      },
+      {
+        header: <div className='last-trades-header'><span>{type}</span>monto</div>,
+        className: 'amount',
+        accessor: 'amount'
+      }
+    ]
+  }
 
-  purchasePositionColumns = this.positionsColumns
-  sellPositionColumns = this.positionsColumns.map(value => value).reverse()
+  positionsColumns () {
+    const [ type, currency ] = this.props.selectedBook.book.split('_')
 
-  purshasePositionHeader = (
-    <div className='purchase-position-header'>
-      <div className='title'>posturas de compra</div>
-      <div className='average'>
-        <span>mxn</span>
-        bid 319,140.00
+    return [
+      {
+        header: '',
+        className: 'percent',
+        accessor: (row) => (
+          <div className='percent' style={{ width: `${row.percent * 100}%` }} />
+        )
+      },
+      {
+        header: 'sum',
+        accessor: 'sum'
+      },
+      {
+        header: <div className='btc-amount'><span>{type}</span>monto</div>,
+        className: 'amount',
+        accessor: 'btcAmount'
+      },
+      {
+        header: <div className='mxn-value'><span>{currency}</span>valor</div>,
+        className: 'mxn-value',
+        accessor: (row) => row.mxnValue.toLocaleString({ style: 'currency', currency: currency, minimumFractionDigits: 2 })
+      },
+      {
+        header: <div className='mxn-price'><span>{currency}</span>precio</div>,
+        className: 'mxn-price',
+        accessor: (row) => row.mxnPrice.toLocaleString({ style: 'currency', currency: currency, minimumFractionDigits: 2 })
+      }
+    ]
+  }
+
+  purshasePositionHeader () {
+    const bid = floatStringToLocaleString(this.props.selectedBook.bid)
+    const currency = this.props.selectedBook.book.split('_')[1]
+
+    return (
+      <div className='purchase-position-header'>
+        <div className='title'>posturas de compra</div>
+        <div className='average'>
+          <span>{currency}</span>
+          bid {bid}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 
-  sellPositionHeader = (
-    <div className='sell-position-header'>
-      <div className='average'>
-        bid 319,140.00
-        <span>mxn</span>
+  sellPositionHeader () {
+    const ask = floatStringToLocaleString(this.props.selectedBook.ask)
+    const currency = this.props.selectedBook.book.split('_')[1]
+
+    return (
+      <div className='sell-position-header'>
+        <div className='average'>
+          {ask} ask
+          <span>{currency}</span>
+        </div>
+        <div className='title'>posturas de venta</div>
       </div>
-      <div className='title'>posturas de venta</div>
-    </div>
-  )
+    )
+  }
 
   render () {
+    const purshasePositionHeader = this.purshasePositionHeader()
+    const sellPositionHeader = this.sellPositionHeader()
+    const lastTradesColumns = this.lastTradesColumns()
+    const purchasePositionColumns = this.positionsColumns()
+    const sellPositionColumns = this.positionsColumns().reverse()
+
     return (
       <div className='page'>
         <TheHeader page={this.props.page} />
@@ -114,20 +136,20 @@ class Home extends Component {
           <Table
             className='last-trades'
             header='últimos trades'
-            columns={this.lastTradesColumns}
+            columns={lastTradesColumns}
             data={lastTradesData}
           />
           <div className='positions'>
             <Table
               className='purchase-position'
-              header={this.purshasePositionHeader}
-              columns={this.purchasePositionColumns}
+              header={purshasePositionHeader}
+              columns={purchasePositionColumns}
               data={purchasePositionData}
             />
             <Table
               className='sell-position'
-              header={this.sellPositionHeader}
-              columns={this.sellPositionColumns}
+              header={sellPositionHeader}
+              columns={sellPositionColumns}
               data={purchasePositionData}
             />
           </div>

6eb3b778e43c425c3b46224ad46b18520b70c089
Michael Villalba Sotelo <visomi.dev@gmail.com>
2018-05-26 23:31:43 -0500

    first aprox for market chart

--- src/components/MarketChart/index.jsx
+++ src/components/MarketChart/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import moment from 'moment-timezone'
 import classnames from 'classnames'
 
 import { floatStringToLocaleString } from '../../lib/utils'
@@ -8,6 +9,8 @@ import './animations.css'
 
 const CURRENCY_POSITION = 1
 const EMPTY_DATA = 0
+const NORMALIZE_INDEX = 1
+const CHART_PADDING = 20
 
 class MarketChart extends Component {
   constructor (props) {
@@ -19,14 +22,20 @@ class MarketChart extends Component {
 
   componentDidMount () {
     if (this.props.data.length === EMPTY_DATA) return false
+    const { data } = this.props
+    const chartHeight = this.refs.chart.height - CHART_PADDING
+    const widthInitial = CHART_PADDING
+    const [ firstTrade ] = data
+    const highestPrice = data.reduce((reducer, trade) => trade.price > reducer ? trade.price : reducer, 0)
     const context = this.refs.chart.getContext('2d')
-    context.fillStyle = 'white'
+    context.strokeStyle = '#fff'
     context.beginPath()
-    context.moveTo(0, 100)
-    context.lineTo(100, 20)
-    context.lineTo(110, 30)
-    context.lineTo(140, 40)
-    context.lineTo(150, 10)
+    context.moveTo(widthInitial, chartHeight)
+    this.props.data.forEach(trade => {
+      const x = moment(firstTrade.createdAt).unix() - moment(trade.createdAt).unix()
+      const y = highestPrice - trade.price
+      context.lineTo(x, y)
+    })
     context.stroke()
   }
 
@@ -38,8 +47,13 @@ class MarketChart extends Component {
     const { expanded } = this.state
     const { book, data, className } = this.props
     const [ lastTrade ] = data
+    const firstTrade = data[data.length - NORMALIZE_INDEX]
     const currency = book.book.split('_')[CURRENCY_POSITION]
     const price = currency === 'mxn' ? floatStringToLocaleString(lastTrade.price, { minimumFractionDigits: 2 }) : lastTrade.price
+
+    const highestPrice = data.reduce((reducer, trade) => trade.price > reducer ? trade.price : reducer, 0)
+    const duration = moment(lastTrade.createdAt).unix() - moment(firstTrade.createdAt).unix()
+
     return (
       <div className={classnames('market-chart', className, { expanded })} onClick={this.toggle.bind(this)}>
         <div className='header'>
@@ -48,10 +62,10 @@ class MarketChart extends Component {
             <i className='material-icons'>{lastTrade.makerSide === 'sell' ? 'arrow_drop_up' : 'arrow_drop_down' }</i>
             {price} {currency}
           </div>
-          <div className='time'>{lastTrade.createdAt}</div>
+          <div className='time'>{moment(lastTrade.createdAt).tz('America/Mexico_City').format('h:mm a')}</div>
         </div>
         <div className='chart'>
-          <canvas ref='chart' />
+          <canvas ref='chart' height={highestPrice + CHART_PADDING} width={duration + CHART_PADDING} />
         </div>
       </div>
     )

be72bc4219734b75715cd48ede38a99bccbb2e95
Antonio Ramirez <tamatao@gmail.com>
2018-05-28 23:51:27 -0500

    Agregado header principal

--- src/Components/Navbar_main.js
+++ src/Components/Navbar_main.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 class Navbar_main extends Component {
 	render() {
 		return <nav class="navbar header-nav fixed-top">
-			<a class="navbar-brand" href="#">
-    			<img src="/images/svg/bitso_logo.svg" width="110" height="36" alt=""/>
-  			</a>
+			<div class="logo">
+				<a class="navbar-brand" href="#">
+	    			<img src="/images/svg/bitso_logo.svg" width="110" height="36" alt=""/>
+	  			</a>
+	  			<div class="exchange">Exchange</div>
+  			</div>
+  			<div class="currentvalue">1 BTC = 000,000 MXN</div>
+  			<div class="infouser">
+  				<ul>
+  					<li>Wallet <img class="icon_dropdown" src="/images/svg/icon_dropdown.svg" width="10"/></li>
+  					<li>Exchange <img class="icon_dropdown" src="/images/svg/icon_dropdown.svg" width="10"/></li>
+  					<li>Ayuda</li>
+  					<li class="usuario"><div class="image-profile"><div></div></div>Usuario <img class="icon_dropdown" src="/images/svg/icon_dropdown.svg" width="10"/></li>
+  					<li></li>
+  				</ul>
+  			</div>
 		</nav>;
 	}
 }

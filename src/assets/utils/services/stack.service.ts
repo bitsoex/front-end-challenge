802d979c99cda8ae77b9bebae9234ced33e2f034
Jose Epifanio <jose.epifanio90@gmail.com>
2018-05-30 18:35:19 -0500

    Add functions to service

--- src/assets/utils/services/stack.service.ts
+++ src/assets/utils/services/stack.service.ts
@@ -1,8 +1,34 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { HttpClient } from "selenium-webdriver/http";
+import { Observable } from "rxjs/Observable";
+import { ConstantsManager } from "../constants";
+import { IBooks } from "../DTO/books.interface";
+import { ITicker } from "../DTO/ticker.interface";
+import { ITrades } from "../DTO/trades.interface";
 
 @Injectable()
 export class StackService {
-    private baseUrl: String = "";
     constructor(private http: HttpClient) {}
+
+    public getAvailableBooks(): Observable<IBooks[]> {
+        return this.http.get(ConstantsManager.AVAILABLE_BOOKS)
+            // .map((res: Response) => res.payload )
+            .catch(this.handleError);
+    }
+
+    public getTicker(): Observable<ITicker[]> {
+        return this.http.get(ConstantsManager.TICKER)
+            // .map((res: Response) => res.payload )
+            .catch(this.handleError);
+    }
+
+    public getTrades(): Observable<ITrades[]> {
+        return this.http.get(ConstantsManager.TRADES)
+            // .map((res: Response) => res.payload )
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any) {
+        return Observable.throw(error || "Server error");
+    }
 }

5af707e8b313514f0e807babf00d569853f7f49e
Caleb Rizo <kaleb.rizzo@gmail.com>
2018-06-08 02:12:05 -0500

    Adds chartbuttons

--- code/src/components/ExchangePage/Chart/Chart.js
+++ code/src/components/ExchangePage/Chart/Chart.js
@@ -32,11 +32,7 @@ class Chart extends Component {
   render() {
     return (
       <div className="chart-container">
-        <div className="chart-option-toggles">
-          <div>Option Toggles</div>
-          <div>Option Toggles</div>
-          <div>Option Toggles</div>
-        </div>
+        <ChartOptionsContainer />
         <div ref={this.chartCanvasRef} className="chart-canvas">
           <ChartComposer
             data={API_DATA}
@@ -50,3 +46,65 @@ class Chart extends Component {
 }
 
 export default Chart;
+
+function ChartOptionsContainer() {
+  return (
+    <div className="chart-options-container">
+      <ChartOptions>
+        <ChartDropdown>
+          <ChartButton id="chart-select-type">
+            <img className="candles-icon" src="img/SVG/icon_candles.svg" alt="candles" />
+          </ChartButton>
+        </ChartDropdown>
+        <ChartDropdown label="Periodo">
+          <ChartButton>
+            <span className="label">3d</span>
+          </ChartButton>
+        </ChartDropdown>
+        <ChartDropdown label="Intervalo">
+          <ChartButton>
+            <span className="label">1h</span>
+          </ChartButton>
+        </ChartDropdown>
+      </ChartOptions>
+      <ChartOptions>
+        <ChartZoom />
+      </ChartOptions>
+    </div>
+  );
+}
+
+function ChartOptions({ children }) {
+  return (
+    <div className="chart-options">
+      {children}
+    </div>
+  );
+}
+
+function ChartDropdown({ label, children }) {
+  return (
+    <div className="chart-dropdown">
+      {label ? <span className="chart-dropdown-label">{label}</span> : null}
+      {children}
+    </div>
+  );
+}
+
+function ChartButton({ children }) {
+  return (
+    <div className="chart-button">
+      {children}
+      <img className="dropdown-icon" src="img/SVG/icon_dropdown.svg" alt="dropdown icon" />
+    </div>
+  );
+}
+
+function ChartZoom() {
+  return (
+    <div className="chart-zoom">
+      <span className="out">-</span>
+      <span className="in">+</span>
+    </div>
+  );
+}

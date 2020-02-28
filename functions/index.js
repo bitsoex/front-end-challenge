7798f815c068dd280b91be8664ab6e3576f3ef9f
Jaime López <loama18@gmail.com>
2020-02-27 21:06:51 -0600

    update

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,15 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
 const axios = require('axios')
 
 exports.bitsoTicker = functions.https.onRequest((request, response) => {
   response.set('Access-Control-Allow-Origin', '*')
 
   var ticker = request.url.split('book=')[1]
+  // console.log(ticker)
   if (ticker !== null) {
     axios.get('https://api.bitso.com/v3/ticker/?book=' + ticker)
     .then(data => {
-      // console.log(data.data)
+      console.log(data.data)
       response.send(data.data)
       return 'a'
     })
